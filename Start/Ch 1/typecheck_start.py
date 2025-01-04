0dfd7bf43c38bfd53d24be546cbf55258215dac4
Benjamin Patch <ben@benjaminpatch.com>
2025-01-03 16:25:27 -0800

    Check for class types.

--- Start/Ch 1/typecheck_start.py
+++ Start/Ch 1/typecheck_start.py
@@ -19,9 +19,16 @@ n1 = Newspaper("The Washington Post")
 n2 = Newspaper("The New York Times")
 
 # TODO: use type() to inspect the object type
+print(type(b1))
+print(type(n1))
 
 
 # TODO: compare two types together
-
+print(type(b1) == type(b2))
+print(type(b1) == type(n1))
 
 # TODO: use isinstance to compare a specific instance to a known type
+print(isinstance(b1,Book))
+print(isinstance(n1,Book))
+# is this an istance of Python's built-in object class
+print(isinstance(n2,object))
